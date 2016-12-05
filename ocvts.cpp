2d5f0f200499f07bcb3cc694d16222a6c5b11714
liangfengchen <liangfengchen@yahoo.com>
2016-12-05 20:37:37 +0800

    Update ocvts.cpp

--- ocvts.cpp
+++ ocvts.cpp
@@ -5,14 +5,14 @@ using namespace cv;
  
 int main( int argc, char** argv )
 {
-        IplImage* pImg; 															//声明IplImage指针
+        IplImage* pImg; 															                                       //声明IplImage指针
         if( argc == 2 && (pImg = cvLoadImage( argv[1], 1)) != 0 )    {		     	//载入图像
-                cvNamedWindow( "Image", 1 );										//创建窗口
+                cvNamedWindow( "Image", 1 );										                        //创建窗口
                 cvResizeWindow("Image", pImg->width, pImg->height);
-                cvShowImage( "Image", pImg );										//显示图像
-                cvWaitKey(0); 														//等待按键
-                cvDestroyWindow( "Image" );											//销毁窗口
-                cvReleaseImage( &pImg ); 											//释放图像
+                cvShowImage( "Image", pImg );										                       //显示图像
+                cvWaitKey(0); 														                                  //等待按键
+                cvDestroyWindow( "Image" );											                        //销毁窗口
+                cvReleaseImage( &pImg ); 											                          //释放图像
                 return 0;
         }
  
